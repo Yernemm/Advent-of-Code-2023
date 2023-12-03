f87c27071cee9cc12c4eac4efff7b414609ad4af
Yernemm <yrnemmizer@gmail.com>
2023-12-03 20:15:29 +0000

    no idea why the other one wasn't working but here we go

---
+++ 3/3-1-again.py
@@ -0,0 +1,53 @@
+inp = open('3.txt', 'r')
+# list of lines
+text = inp.readlines()
+
+total = 0
+
+
+def isSymbol(char):
+    return (not char.isnumeric()) and (not char == '.')
+
+def isPartDigit(x, y):
+    flag = False
+    flag = flag or isSymbol(text[y-1][x-1])
+    flag = flag or isSymbol(text[y-1][x])
+    flag = flag or isSymbol(text[y-1][x+1])
+    
+    flag = flag or isSymbol(text[y][x-1])
+    flag = flag or isSymbol(text[y][x+1])
+    
+    flag = flag or isSymbol(text[y+1][x-1])
+    flag = flag or isSymbol(text[y+1][x])
+    flag = flag or isSymbol(text[y+1][x+1])
+    return flag
+
+#pre-process input
+for idx, line in enumerate(text):
+    text[idx] = text[idx].strip()
+    text[idx] = "." + text[idx] + "."
+
+line = ''
+for i in text[0]:
+    line += '.'
+
+text.insert(0,line)
+text.append(line)
+
+#check numbers
+for y, line in enumerate(text):
+    number = ""
+    isPart = False
+    for x, char in enumerate(line):
+        if char.isnumeric():
+            number += char
+            isPart = isPart or isPartDigit(x, y)
+        elif len(number) > 0:
+            print(number) 
+            if(isPart):
+                total += int(number)
+                isPart = False
+            number = ""
+
+print(text)
+print(total)
